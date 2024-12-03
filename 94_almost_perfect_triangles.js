e46c2898eb53595748505fc9c45abba335431657
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-12-03 17:49:06 -0500

    blarg

--- 94_almost_perfect_triangles.js
+++ 94_almost_perfect_triangles.js
@@ -7,21 +7,55 @@ const getTriangles = () => {
   for (let i = 2; i <= MAX_SIDE; i++) {
     const s1 = i + 1;
     const p1 = 2 * i + s1;
-    const partialHeronArea = (3 * i * i - 2 * i - 1) / 16;
+    const partialHeronArea = ((3 * i + 1) * (i - 1)) / 16;
 
     const s2 = i - 1;
     const p2 = 2 * i + s2;
-    const partialHeronArea2 = (3 * i * i + 2 * i - 1) / 16;
+    const partialHeronArea2 = ((3 * i - 1) * (i + 1)) / 16;
 
-    const max = Math.ceil((Math.pow(3, 0.5) * (i + 1)) / 4)
-    let j = max + 1;
+    const max = Math.ceil((Math.pow(3, 0.5) * (i + 1)) / 4);
+    let j = max;
 
-    if(i === 17){
-        console.log(i, s1,s2,"partialHeronArea", partialHeronArea, "partialHeronArea2", partialHeronArea2, "max", max)
-    }
-    while (Math.pow(j, 2) >= Math.min(partialHeronArea2, partialHeronArea)) {
+    while (Math.pow(j, 2) >= partialHeronArea) {
       squared = Math.pow(j, 2);
-    if(i === 17) console.log("squared", squared)
+      if (i === 241) {
+        console.log(
+          "i",
+          i,
+          "squared",
+          squared,
+          "partialHeronArea",
+          partialHeronArea,
+          partialHeronArea * (i + 1) * (i + 1),
+          partialHeronArea * (i - 1) * (i - 1),
+          "partialHeronArea2",
+          partialHeronArea2,
+
+          partialHeronArea2 * (i + 1) * (i + 1),
+          partialHeronArea2 * (i - 1) * (i - 1),
+          "max",
+          max
+        );
+      }
+      if (i === 17) {
+        console.log(
+          "i",
+          i,
+          "squared",
+          squared,
+          "partialHeronArea",
+          partialHeronArea,
+          partialHeronArea * (i + 1) * (i + 1),
+          partialHeronArea * (i - 1) * (i - 1),
+
+          "partialHeronArea2",
+          partialHeronArea2,
+          partialHeronArea2 * (i + 1) * (i + 1),
+          partialHeronArea2 * (i - 1) * (i - 1),
+          "max",
+          max
+        );
+      }
       if (squared === partialHeronArea) {
         if (p1 <= BILLION) {
           sum += p1;
